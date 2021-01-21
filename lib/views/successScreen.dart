a65d9ab7af361a49e86731d2851c5191259d1c6d
Suresh Kumar Chaturvedi <suresh007kc@gmail.com>
2021-01-22 01:25:12 +0530

    Added SCSV to Success Screen

--- lib/views/successScreen.dart
+++ lib/views/successScreen.dart
@@ -9,73 +9,76 @@ class SuccessScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: getGradient(),
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Center(
-            child: Column(
-              children: [
-                // Basil Hut Logo
-                Container(
-                  height: 300,
-                  width: 400,
-                  child: Image.asset('images/BasilHutIcon.png'),
-                ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  // Basil Hut Logo
+                  Container(
+                    height: 300,
+                    width: 400,
+                    child: Image.asset('images/BasilHutIcon.png'),
+                  ),
 
-                // Registration Details Card
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    color: Colors.white,
-                  ),
-                  padding: EdgeInsets.all(25),
-                  margin: EdgeInsets.only(bottom: 70),
-                  width: MediaQuery.of(context).size.width - 40,
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'images/checked.png',
-                        width: 60,
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Text(
-                        "Registration Successful",
-                        style: userInfoTextStyle(),
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      Text(
-                        "Your Registration ID is: ",
-                        style: inputTextFieldStyle(),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Text(
-                        regID,
-                        style: userInfoTextStyle(),
-                      )
-                    ],
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 160,
-                  height: 40,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Let's Go!",
-                      style: TextStyle(fontSize: 20, color: Color(0xffd8f3dc)),
+                  // Registration Details Card
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white,
                     ),
-                    color: getLogoColor(),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(7)),
+                    padding: EdgeInsets.all(25),
+                    margin: EdgeInsets.only(bottom: 70),
+                    width: MediaQuery.of(context).size.width - 40,
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'images/checked.png',
+                          width: 60,
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        Text(
+                          "Registration Successful",
+                          style: userInfoTextStyle(),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Text(
+                          "Your Registration ID is: ",
+                          style: inputTextFieldStyle(),
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        Text(
+                          regID,
+                          style: userInfoTextStyle(),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  ButtonTheme(
+                    minWidth: 160,
+                    height: 40,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Let's Go!",
+                        style:
+                            TextStyle(fontSize: 20, color: Color(0xffd8f3dc)),
+                      ),
+                      color: getLogoColor(),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7)),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
